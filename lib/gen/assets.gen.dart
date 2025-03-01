1b98412a05690ff62f61b47fa41708758fe82a05
Alvish Ramani <alvishramani.dev@gmail.com>
2025-03-01 11:33:21 +0530

    Add card assets and update project configuration
    - Added multiple card assets for different card styles (Black Metal, Black PVC, Chameleon, etc.)
    - Updated pubspec.yaml to include new assets and development dependencies
    - Added flutter_gen_runner for asset generation
    - Generated assets.gen.dart for type-safe asset references
    - Cleaned up main.dart boilerplate code

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,275 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsMediasGen {
+  const $AssetsMediasGen();
+
+  /// Directory path: assets/medias/cards
+  $AssetsMediasCardsGen get cards => const $AssetsMediasCardsGen();
+}
+
+class $AssetsMediasCardsGen {
+  const $AssetsMediasCardsGen();
+
+  /// Directory path: assets/medias/cards/Black_Metal
+  $AssetsMediasCardsBlackMetalGen get blackMetal =>
+      const $AssetsMediasCardsBlackMetalGen();
+
+  /// Directory path: assets/medias/cards/Black_PVC
+  $AssetsMediasCardsBlackPVCGen get blackPVC =>
+      const $AssetsMediasCardsBlackPVCGen();
+
+  /// Directory path: assets/medias/cards/Chameleon
+  $AssetsMediasCardsChameleonGen get chameleon =>
+      const $AssetsMediasCardsChameleonGen();
+
+  /// Directory path: assets/medias/cards/Glitter
+  $AssetsMediasCardsGlitterGen get glitter =>
+      const $AssetsMediasCardsGlitterGen();
+
+  /// Directory path: assets/medias/cards/Glow
+  $AssetsMediasCardsGlowGen get glow => const $AssetsMediasCardsGlowGen();
+
+  /// Directory path: assets/medias/cards/HBA
+  $AssetsMediasCardsHBAGen get hba => const $AssetsMediasCardsHBAGen();
+
+  /// Directory path: assets/medias/cards/Mood
+  $AssetsMediasCardsMoodGen get mood => const $AssetsMediasCardsMoodGen();
+
+  /// Directory path: assets/medias/cards/Pink
+  $AssetsMediasCardsPinkGen get pink => const $AssetsMediasCardsPinkGen();
+
+  /// Directory path: assets/medias/cards/Tortoise
+  $AssetsMediasCardsTortoiseGen get tortoise =>
+      const $AssetsMediasCardsTortoiseGen();
+
+  /// Directory path: assets/medias/cards/White_PVC
+  $AssetsMediasCardsWhitePVCGen get whitePVC =>
+      const $AssetsMediasCardsWhitePVCGen();
+}
+
+class $AssetsMediasCardsBlackMetalGen {
+  const $AssetsMediasCardsBlackMetalGen();
+
+  /// File path: assets/medias/cards/Black_Metal/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Black_Metal/Back.jpg');
+
+  /// File path: assets/medias/cards/Black_Metal/Front.mp4
+  String get front => 'assets/medias/cards/Black_Metal/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsBlackPVCGen {
+  const $AssetsMediasCardsBlackPVCGen();
+
+  /// File path: assets/medias/cards/Black_PVC/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Black_PVC/Back.jpg');
+
+  /// File path: assets/medias/cards/Black_PVC/Front.mp4
+  String get front => 'assets/medias/cards/Black_PVC/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsChameleonGen {
+  const $AssetsMediasCardsChameleonGen();
+
+  /// File path: assets/medias/cards/Chameleon/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Chameleon/Back.jpg');
+
+  /// File path: assets/medias/cards/Chameleon/Front.mp4
+  String get front => 'assets/medias/cards/Chameleon/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsGlitterGen {
+  const $AssetsMediasCardsGlitterGen();
+
+  /// File path: assets/medias/cards/Glitter/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Glitter/Back.jpg');
+
+  /// File path: assets/medias/cards/Glitter/Front.mp4
+  String get front => 'assets/medias/cards/Glitter/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsGlowGen {
+  const $AssetsMediasCardsGlowGen();
+
+  /// File path: assets/medias/cards/Glow/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Glow/Back.jpg');
+
+  /// File path: assets/medias/cards/Glow/Front.mp4
+  String get front => 'assets/medias/cards/Glow/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsHBAGen {
+  const $AssetsMediasCardsHBAGen();
+
+  /// File path: assets/medias/cards/HBA/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/HBA/Back.jpg');
+
+  /// File path: assets/medias/cards/HBA/Front.jpg
+  AssetGenImage get front =>
+      const AssetGenImage('assets/medias/cards/HBA/Front.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [back, front];
+}
+
+class $AssetsMediasCardsMoodGen {
+  const $AssetsMediasCardsMoodGen();
+
+  /// File path: assets/medias/cards/Mood/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Mood/Back.jpg');
+
+  /// File path: assets/medias/cards/Mood/Front.mp4
+  String get front => 'assets/medias/cards/Mood/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsPinkGen {
+  const $AssetsMediasCardsPinkGen();
+
+  /// File path: assets/medias/cards/Pink/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Pink/Back.jpg');
+
+  /// File path: assets/medias/cards/Pink/Front.mp4
+  String get front => 'assets/medias/cards/Pink/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class $AssetsMediasCardsTortoiseGen {
+  const $AssetsMediasCardsTortoiseGen();
+
+  /// File path: assets/medias/cards/Tortoise/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/Tortoise/Back.jpg');
+
+  /// File path: assets/medias/cards/Tortoise/Front.jpg
+  AssetGenImage get front =>
+      const AssetGenImage('assets/medias/cards/Tortoise/Front.jpg');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [back, front];
+}
+
+class $AssetsMediasCardsWhitePVCGen {
+  const $AssetsMediasCardsWhitePVCGen();
+
+  /// File path: assets/medias/cards/White_PVC/Back.jpg
+  AssetGenImage get back =>
+      const AssetGenImage('assets/medias/cards/White_PVC/Back.jpg');
+
+  /// File path: assets/medias/cards/White_PVC/Front.mp4
+  String get front => 'assets/medias/cards/White_PVC/Front.mp4';
+
+  /// List of all assets
+  List<dynamic> get values => [back, front];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsMediasGen medias = $AssetsMediasGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
